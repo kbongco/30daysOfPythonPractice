ae714ad33ee6fad9685b5adcc5c2256fcbb73fe8
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-12-27 12:52:36 -0500

    day 1 and 2

---
+++ day1/day1.py
@@ -0,0 +1,37 @@
+# Day 2: 30 Days of Python Programming
+import math
+
+first_name = 'Chibi'
+last_name = 'Skye'
+full_name = 'Chibi Skye'
+country = 'USA'
+city = 'New York'
+age = 30
+year = 2023
+is_married = False
+is_true = True or False
+is_light_on = 'yes'
+# multiple, variables = 'this', 'is'
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(len(first_name))
+print(len(last_name))
+print(len(first_name) > len(last_name))
+num_one = 5
+num_two = 4
+
+total = num_one + num_two 
+diff  = num_one - num_two 
+product = num_one * num_two 
+division = num_one / num_two 
+remainder = num_two % num_one 
+exp = num_one ** num_two
+floor = num_two // num_one 
+
+radius = 30
+area_of_circle = math.pi * radius ** 2 
+circum_of_circle = 2 * math.pi * 30
+print(area_of_circle)
+print(circum_of_circle)
---
+++ day1/day2.py
@@ -0,0 +1,6 @@
+# Technically day 3  but day 2 for this purpose
+
+age = 31
+height = 5.7
+complexed = 5 + 1j
+height = input()
