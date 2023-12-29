10581fb751c1e7f4c19b82698dc5fed8ff4f46f0
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-12-29 12:06:10 -0500

    day2

---
+++ day2/day3.py
@@ -0,0 +1,20 @@
+thirty = 'Thirty'
+days = 'Days'
+of = 'Of'
+python = 'Python'
+space = ''
+
+print(thirty + days + of + python)
+coding = 'Coding'
+fore = 'For'
+all = 'All'
+
+company = coding + space +  fore + space + all
+print(company)
+print(len(company))
+print(company.upper())
+print(company.lower())
+print(company[6:])
+print(company.find('Coding'))
+
+  
